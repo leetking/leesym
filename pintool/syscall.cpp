5b8fdace8d9edd31e0babd077e5aaca112bae84d
alpha0x00 <leetiankai@gmail.com>
2022-02-26 15:56:49 +0800

    Taint via read

--- pintool/syscall.cpp
+++ pintool/syscall.cpp
@@ -24,24 +24,54 @@ UINT64* llseekResult;
 UINT64 taintMemoryStart;
 UINT64 mmapSize;
 UINT64 targetFileFd=0xFFFFFFFF;
+string targetFileName;
 
 
 namespace {
-    enum { SYSCALL_NONE = (unsigned)-1 };
-    unsigned syscall_number = SYSCALL_NONE;
+enum {
+    SYSCALL_NONE = (unsigned)-1,
+    INVALID_FD = -1,
+};
+unsigned syscall_number = SYSCALL_NONE;
 
-    struct {
-        ADDRINT heap_ptr;
-        ADDRINT new_heap_ptr;
-    } brk_info;
+struct {
+    bool started = false;
+    int target_fd = STDIN_FILENO;   // 默认从标准输入读
+    UINT64 offset = 0;
+} taint;
 
-    struct {
-        ADDRINT start;
-        size_t length;
-    } munmap_info;
+struct {
+    char* fname;
+    int flags;
+} open_info;
+
+struct {
+    int fd = INVALID_FD;
+    ADDRINT addr = 0;
+    ADDRINT size = 0;
+} read_info;
+
+struct {
+    int fd = INVALID_FD;
+} close_info;
+
+struct {
+    ADDRINT heap_ptr = 0;
+    ADDRINT new_heap_ptr = 0;
+} brk_info;
+
+struct {
+    ADDRINT start = 0;
+    size_t length = 0;
+} munmap_info;
+} // end namespace
+
+static inline
+bool string_equal(char const* str1, char const* str2)
+{
+    return strcmp(str1, str2) == 0;
 }
 
-string targetFileName;
 
 /**
  * num: syscall no
@@ -75,50 +105,42 @@ VOID SysBefore(ADDRINT ip, ADDRINT num, ADDRINT arg0, ADDRINT arg1, ADDRINT arg2
         munmap_info.start = arg0;
         munmap_info.length = arg1;
         break;
-    }
 
-    // read(fd, mem, size)
-    if(num == __NR_read) {
-        logfile << "[READ FILE]\t";
-        logfile << hex << "0x" << ip << ":\tfd: " << arg0 << endl;
+    // 类似于 open, 打开相对于文件夹 at fd 下的文件
+    case __NR_openat:
+        logfile << "[openat]\tat fd: " << arg0
+            << " fname: " << (arg1? (char*)arg1: "(null)")
+            << " flags: " << hex << arg2 << endl;
+        arg0 = arg1;
+        arg1 = arg2;
+    case __NR_open:
+        logfile << "[open]\t\t" << "fname: " << (arg0? (char*)arg0: "(null)") << " ";
+        logfile << hex << "flags: " << arg1 << endl;
+        open_info.fname = (char*)arg0;
+        open_info.flags = arg1;
+        break;
 
-        taintMemoryStart = static_cast<INT64>(arg1);
+    case __NR_close:
+        logfile << "[close]\t\tfd: " << arg0 << endl;
+        close_info.fd = arg0;
+        break;
 
-        // 只能处理一个文件：而且只能是目标程序处理的文件
-        if(arg0 == targetFileFd || arg0 == 0) {
-            isTargetFileRead = true;
-            isTaintStart = true;
-        }
+    // read(fd, addr, size)
+    case __NR_read:
+        logfile << "[read]\t\tfd: " << arg0
+            << " addr: " << hex << arg1
+            << " size: " << dec << arg2 << endl;
+        read_info.fd = arg0;
+        read_info.addr = arg1;
+        read_info.size = arg2;
+        break;
 
-    } else if(num == __NR_open) {
-        logfile << "[OPEN FILE]\t";
+    case __NR_lseek:
+        logfile << hex << "[lseek]\t\tfd: " << arg0 << " offset: " << arg1 << " whence: " << arg2 << endl;
+        break;
+    } // switch (syscall_number)
 
-        logfile << hex << "0x" << ip << ":\t" << (char*)arg0 << endl;
-
-        // 打开目标程序读取的文件
-        if(strstr((char*)arg0, targetFileName.c_str()) != NULL){
-            isTargetFileOpen = true;
-            logfile << "\tOpen target file" << endl;
-
-            isTaintStart = true;
-        }
-
-        // 动态链接程序，首先会打开 libc.so 加载动态链接库
-        if(strstr((char*)arg0, "libc.so") != NULL) {
-            isLibcSO = true;
-        }
-
-    } else if(num == __NR_close) {
-        logfile << hex << "[CLOSE FILE]\t\tfd: " << arg0 << endl;
-
-        if(arg0 == targetFileFd){
-            targetFileFd = 0xFFFFFFFF;
-        }
-
-        if(isLibcSO == true){
-            isTaintStart = true;
-        }
-    } else if(num == __NR_lseek){
+    if(num == __NR_lseek) {
         logfile << hex << "[LSEEK FILE]\t\tfd: " << arg0 << " offset: " << arg1 << " whence: " << arg2 << endl;
 
         if(arg0 == targetFileFd){
@@ -152,14 +174,14 @@ VOID SysBefore(ADDRINT ip, ADDRINT num, ADDRINT arg0, ADDRINT arg1, ADDRINT arg2
 #endif
 }
 
-#define syscall_failed(ret) (((ADDRINT)-1) == ret)
+#define syscall_failed(ret) (((ADDRINT)-1) == (ret))
 
 VOID SysAfter(ADDRINT ret)
 {
+    UINT64 size;
+
     switch (syscall_number) {
     case __NR_brk:
-        //BUG_ON(brk_info.new_heap_ptr == nullptr);
-        // syscall failed
         if (syscall_failed(ret))
             break;
         // 减少内存
@@ -178,6 +200,63 @@ VOID SysAfter(ADDRINT ret)
             break;
         removeTaintBlock(munmap_info.start, munmap_info.length);
         logfile << hex << "[munmap]\t\tremove from: " << munmap_info.start << " len: " << munmap_info.length << endl;
+        munmap_info.start = munmap_info.length = 0;
+        break;
+
+    case __NR_openat:
+        if (syscall_failed(ret))
+            break;
+        logfile << "[openat]\topened fd: " << ret << endl;
+    case __NR_open:
+        if (syscall_failed(ret))
+            break;
+        logfile << "[open]\t\topened fd: " << ret << endl;
+        // 已经开始污点分析了
+        if (taint.started)
+            break;
+        // 打开目标程序读取的文件
+        if (string_equal(open_info.fname, targetFileName.c_str())) {
+            if (open_info.flags == O_RDONLY || open_info.flags == O_RDWR) {
+                logfile << "[TAINT] open input file at " << ret << endl;
+                taint.target_fd = ret;
+            }
+        }
+        open_info.fname = nullptr;
+        open_info.flags = 0x0;
+        break;
+
+    case __NR_read:
+        if (syscall_failed(ret))
+            break;
+        if (read_info.fd != taint.target_fd)
+            break;
+        if (!taint.started) {
+            logfile << "BEGIN TAINT!!" << endl;
+            taint.started = true;
+            isTaintStart = true;
+        }
+        size = ret;
+        for (UINT64 i = 0; i < size; ++i)
+            addTaintByte(read_info.addr + i, taint.offset++);
+        logfile << "[TAINT]\t\taddr: " << hex << read_info.addr
+            << " size: " << dec << ret
+            << " offset: " << taint.offset - size << endl;
+        read_info.fd = INVALID_FD;
+        read_info.addr = 0x0;
+        read_info.size = 0;
+        break;
+
+    case __NR_close:
+        if (syscall_failed(ret))
+            break;
+        if (taint.target_fd == close_info.fd) {
+            logfile << "[TAINT]\tClose input file fd " << taint.target_fd << endl;
+            taint.target_fd = STDIN_FILENO;
+        }
+        close_info.fd = INVALID_FD;
+        break;
+
+    case __NR_mmap:
         break;
 
     case SYSCALL_NONE:
@@ -186,31 +265,7 @@ VOID SysAfter(ADDRINT ret)
     }
     syscall_number = SYSCALL_NONE;
 
-    if(isTargetFileOpen && ret >= 0){
-        targetFileFd = ret;
-        isTargetFileOpen = false;
-        globalOffset = 0;
-        logfile << "\topen file descriptor " << targetFileFd << endl;
-    }
-
-    if(isTargetFileRead && ret >= 0){
-        isTargetFileRead = false;
-
-        UINT64 size = ret;
-
-        for (UINT64 i = 0; i < size; i++){
-            // 按字节标记内存被污染
-            addTaintByte(taintMemoryStart + i, globalOffset);
-
-            globalOffset++;
-        }
-
-        logfile << "[TAINT]\t\t\t0x" << size << " bytes tainted from ";
-        logfile << hex << "0x" << taintMemoryStart << " to 0x" << taintMemoryStart+size;
-        logfile << " by file offset 0x" << globalOffset-size << " (via read)" << endl;
-    }
-
-    if(isLseekCalled == true){
+    if (isLseekCalled == true) {
         isLseekCalled = false;
         globalOffset = ret;
 
@@ -227,7 +282,7 @@ VOID SysAfter(ADDRINT ret)
     // mmap2 内存映射方式读取
     if(isTargetFileMmap2){
         isTargetFileMmap2 = false;
-        
+
         if(ret != 0xFFFFFFFF){
             UINT64 mmapResult = ret;
 
