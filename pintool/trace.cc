95f30c5e227cfab0f465a82097d84e27fa1ab5b6
alpha0x00 <leetiankai@gmail.com>
2022-03-08 19:12:58 +0800

    Trace lea and jmp with new style

--- pintool/trace.cc
+++ pintool/trace.cc
@@ -236,7 +236,7 @@ void tracelog_leamem(UINT64 insaddr, string const& insdis,
         UINT64 disp,
         UINT32 size)
 {
-    // addr.lea ecx, [eax+4*ebx+0x22].{base offsets}{idx offsets}.bval.scale.ival.disp.
+    // addr.lea ecx, [eax+4*ebx+0x22].{base offsets}{idx offsets}.bval.ival.scale.disp.
     UINT32 bsize = size;
     UINT32 isize = size;
     trace << hex << "0x" << insaddr << "." << insdis << ".";
@@ -255,9 +255,9 @@ void tracelog_leamem(UINT64 insaddr, string const& insdis,
     trace << ".";
     dump_value(bval, bsize);
     trace << ".";
-    dump_value(scale, size);
-    trace << ".";
     dump_value(ival, isize);
+    trace << ".";
+    dump_value(scale, size);
     trace << ".";
     dump_value(disp, isize);
     trace << "." << endl;
@@ -283,7 +283,7 @@ void tracelog_jmpmem(UINT64 insaddr, string const& insdis, ADDRINT result,
         UINT64 disp,
         UINT32 size)
 {
-    // addr.jmp [eax+4*ebx+0x22].result.{base offsets}{idx offsets}.bval.scale.ival.disp.
+    // addr.jmp [eax+4*ebx+0x22].result.{base offsets}{idx offsets}.bval.ival.scale.disp.
     UINT32 bsize = size;
     UINT32 isize = size;
     trace << hex << "0x" << insaddr << "." << insdis << ".";
@@ -304,9 +304,9 @@ void tracelog_jmpmem(UINT64 insaddr, string const& insdis, ADDRINT result,
     trace << ".";
     dump_value(bval, bsize);
     trace << ".";
-    dump_value(scale, size);
-    trace << ".";
     dump_value(ival, isize);
+    trace << ".";
+    dump_value(scale, size);
     trace << ".";
     dump_value(disp, isize);
     trace << "." << endl;
